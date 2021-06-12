b92a617802685c3bbb0afe91ab0f93da39a072fa
Eric Christie <ericjchristie@gmail.com>
2021-06-11 21:15:44 -0400

    Implement basic toggler UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { Button, Container, Row } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const dummyOptions = [
+  {
+    name: "Album 1",
+    active: true,
+    enabled: true
+  },
+  {
+    name: "Album 2",
+    active: false,
+    enabled: true
+  },
+  {
+    name: "Album 3",
+    active: false,
+    enabled: false
+  },
+  {
+    name: "Album 4",
+    active: false,
+    enabled: true
+  },
+]
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.getOptions = this.getOptions.bind(this);
+    this.activateOption = this.activateOption.bind(this);
+
+    this.state = {
+      options: []
+    };
+  }
+
+  componentDidMount() {
+    this.getOptions().then(response => {
+      this.setState({
+        options: response
+      });
+    });
+  }
+
+  render() {
+    const buttons = this.state.options.map(option =>
+      <Button key={option.name} block disabled={!option.enabled || option.active} 
+          variant={option.active ? "success" : "secondary"}
+          onClick={(e) => this.activateOption(option, e)}>
+        {option.name}
+      </Button>
+    );
+    return (
+      <Container>
+        <Row>
+          {buttons}
+        </Row>
+      </Container>
+    );
+  }
+
+  getOptions() {
+    return Promise.resolve(dummyOptions);
+  }
+
+  activateOption(option) {
+    let ndx = dummyOptions.findIndex(item => item.name === option.name);
+    dummyOptions.forEach(item => {
+      item.active = false;
+    });
+    dummyOptions[ndx].active = true;
+
+    this.getOptions().then(response => {
+      this.setState({
+        options: response
+      });
+    })
+  }
 }
 
 export default App;
